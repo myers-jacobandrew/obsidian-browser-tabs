eac34524ffb51ccea16f4cf44ddcaaa83490a4a3
Jake <myers.jacobandrew1@gmail.com>
2023-02-20 21:22:49 -0600

    initial commit
    first push.
    
    Probably everything works no problems.
    
    TODOs are check and see if it works and then add more to it.

--- main.ts
+++ main.ts
@@ -1,137 +1,201 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, Notice, MarkdownView, TFile, TFolder, WorkspaceLeaf } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+class TabGroup {
+	protected id: string;
+	private name: string;
+	private color: string;
+	protected groupLeaves: WorkspaceLeaf[] = [];
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	constructor(id: string, name: string, color: string) {
+	  this.id = id;
+	  this.name = name;
+	  this.color = color;
 	}
 
-	onunload() {
-
+	getId() {
+	  return this.id;
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	getName() {
+	  return this.name;
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	setName(name: string) {
+	  this.name = name;
+	}
+
+	getColor() {
+	  return this.color;
+	}
+
+	setColor(color: string) {
+	  this.color = color;
+	  this.updateLeavesColor();
+	}
+
+	addLeafToGroup(leaf: WorkspaceLeaf) {
+	  if (!this.groupLeaves.includes(leaf)) {
+		this.groupLeaves.push(leaf);
+		this.updateLeavesColor();
+	  }
+	}
+
+	removeLeafFromGroup(leaf: WorkspaceLeaf) {
+	  const index = this.groupLeaves.indexOf(leaf);
+	  if (index !== -1) {
+		this.groupLeaves.splice(index, 1);
+		this.updateLeavesColor();
+	  }
+	}
+
+	updateLeavesColor() {
+	  this.groupLeaves.forEach((leaf) => {
+		leaf.setEphemeralState({
+		  backgroundColor: this.color,
+		});
+	  });
+	}
+
+	getGroupLeaves() {
+	  return this.groupLeaves;
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+  
+class ModifiedTabGroup extends TabGroup {
+	constructor(id: string, name: string, color: string, groupLeaves: WorkspaceLeaf[]) {
+	  super(id, name, color);
+	  this.groupLeaves = groupLeaves;
+	}
+  
+	getId() {
+	  return this.id;
+	}
+  
+	addLeafToGroup(leaf: WorkspaceLeaf) {
+	  this.groupLeaves.push(leaf);
+	  leaf.setEphemeralState({ tabGroup: this });
+	  this.updateLeavesColor();
+	}
+  
+	removeLeafFromGroup(leaf: WorkspaceLeaf) {
+	  const leafIndex = this.groupLeaves.indexOf(leaf);
+	  if (leafIndex >= 0) {
+		this.groupLeaves.splice(leafIndex, 1);
+		leaf.setEphemeralState({ tabGroup: undefined });
+		this.updateLeavesColor();
+	  }
+	}
+  
+	updateLeavesColor() {
+	  const leaves = this.getGroupLeaves();
+	  for (let i = 0; i < leaves.length; i++) {
+		leaves[i].setEphemeralState({ backgroundColor: this.getColor });
+	  }
+	}
+  
+	getGroupLeaves() {
+	  return this.groupLeaves;
+	}
+  }
+  
+
+  function getTabGroupData(leaf: WorkspaceLeaf): { id: string } | undefined {
+	const data = leaf.getEphemeralState();
+	return data.tabGroup ? { id: data.tabGroup.getId() } : undefined;
+  }
+
+interface MyLeafData  {
+	tabGroup?: TabGroup;
+}
+
+class TabGroupManager {
+	private groups: ModifiedTabGroup[] = [];
+
+	addGroup(group: ModifiedTabGroup) {
+	  this.groups.push(group);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	getGroupById(id: string) {
+	  return this.groups.find((group) => group.getId() === id);
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+const tabGroupManager = new TabGroupManager();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+function createTabGroup(name: string, color: string, groupLeaves: WorkspaceLeaf[]) {
+	const id = Math.random().toString(36).substring(2);
+	const tabGroup = new ModifiedTabGroup(id, name, color, groupLeaves);
+	tabGroupManager.addGroup(tabGroup);
+	return tabGroup;
+  }
+  
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+function setTabGroup(leaf: WorkspaceLeaf, group: ModifiedTabGroup): void {
+    if (leaf && group) {
+        leaf.setEphemeralState({ tabGroup: group });
+    }
 }
+
+
+  function setLeafGroupData(leaf: WorkspaceLeaf, group: ModifiedTabGroup): void {
+	const data = leaf.getEphemeralState();
+	data.group = group;
+	leaf.setEphemeralState(data);
+  }
+  
+
+  
+
+
+  class WorkspaceTab {
+	leaf: WorkspaceLeaf;
+	group: ModifiedTabGroup;
+  
+	constructor(leaf: WorkspaceLeaf, group: ModifiedTabGroup) {
+	  this.leaf = leaf;
+	  this.group = group;
+	}
+  }
+  
+    function setTabGroupData(leaf: WorkspaceLeaf, group: TabGroup): void {
+	// Cast the leaf to a type that includes the tabGroup property
+	const myLeaf = leaf as WorkspaceLeaf & MyLeafData;
+	// Store the entire TabGroup object as a property on the WorkspaceLeaf instance
+	myLeaf.tabGroup = group;
+  }
+
+  function clearTabGroupData(leaf: WorkspaceLeaf) {
+	const data = leaf.getEphemeralState();
+	delete data.tabGroup;
+	leaf.setEphemeralState(data);
+  }
+  
+  
+  	function linkWithTab(leaf: WorkspaceLeaf, groupLeaves: WorkspaceLeaf[]) {
+	const group = createTabGroup("My Tab Group", "#ff0000", groupLeaves) as ModifiedTabGroup;
+	group.addLeafToGroup(leaf);
+	setTabGroupData(leaf, group);
+  
+	this.registerEvent(this.app.workspace.on("layout-ready", () => {
+		const unlinkedLeaves = this.app.workspace.getLeavesOfType("unlinked");
+		for (const leaf of unlinkedLeaves) {
+			const tabGroupData = getTabGroupData(leaf);
+			if (tabGroupData) {
+			  const tabGroup = tabGroupManager.getGroupById(tabGroupData.id);
+			  if (tabGroup) {
+				tabGroup.removeLeafFromGroup(leaf);
+			  }
+			  clearTabGroupData(leaf);		}
+	  }}));
+  
+	leaf.setEphemeralState({
+	  linkWithTab: {
+		group,
+		groupLeaves: [leaf],
+	  },
+	});
+  
+	new Notice('Linked with Tab');
+  }
+  
